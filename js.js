581cdfb79ea4b5096b69e4b345e4233251643630
Marcin Karwacki <kreatywny20@wp.pl>
2018-11-05 19:17:17 +0100

    swapping points

--- js.js
+++ js.js
@@ -1,6 +1,12 @@
 let cities = [];
 let totalCities = 5;
 
+function swap(a, i, j) {
+    let temp = a[i];
+    a[i] = a[j];
+    a[j] = temp;
+}
+
 function setup() {
     createCanvas(800, 600);
     for (let i = 0; i < totalCities; i++) {
@@ -15,4 +21,16 @@ function draw() {
         ellipse(cities[i].x, cities[i].y, 8, 8)
     }
 
+    stroke(255);
+    strokeWeight(1);
+    noFill();
+    beginShape();
+    for (let i = 0; i < cities.length; i++) {
+        vertex(cities[i].x, cities[i].y)
+    }
+    endShape();
+
+    let i = floor(random(cities.length));
+    let j = floor(random(cities.length));
+    swap(cities, i, j);
 }
