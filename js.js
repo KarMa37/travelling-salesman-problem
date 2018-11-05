27578b94bb5a98abcaed0aa6fd2caf1abe58ea40
Marcin Karwacki <kreatywny20@wp.pl>
2018-11-05 19:41:31 +0100

    calc & log smallest route

--- js.js
+++ js.js
@@ -1,5 +1,6 @@
 let cities = [];
 let totalCities = 5;
+let recordDistance;
 
 function swap(a, i, j) {
     let temp = a[i];
@@ -7,11 +8,20 @@ function swap(a, i, j) {
     a[j] = temp;
 }
 
+function calcDistance(points) {
+    let sum = 0;
+    for (let i = 0; i < points.length - 1; i++) {
+        sum += dist(points[i].x, points[i].y, points[i + 1].x, points[i + 1].y);
+    }
+    return sum;
+}
+
 function setup() {
     createCanvas(800, 600);
     for (let i = 0; i < totalCities; i++) {
         cities[i] = createVector(random(width), random(height));
     }
+    recordDistance = calcDistance(cities);
 }
 
 function draw() {
@@ -33,4 +43,10 @@ function draw() {
     let i = floor(random(cities.length));
     let j = floor(random(cities.length));
     swap(cities, i, j);
+
+    let dist = calcDistance(cities);
+    if (dist < recordDistance) {
+        recordDistance = dist;
+        console.log(recordDistance);
+    }
 }
